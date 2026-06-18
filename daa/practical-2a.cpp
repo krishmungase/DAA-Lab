6a8940723c309f5f64a6900875f9e431db6195ab
krishmungase <mungasekrishna8@gmail.com>
2026-06-18 22:10:36 +0530

    refactor: inline fractional knapsack logic into main function for clarity

--- daa/practical-2a.cpp
+++ daa/practical-2a.cpp
@@ -2,26 +2,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare(pair<int,int> a, pair<int,int> b) {
-    return (double)a.first / a.second > (double)b.first / b.second;
-}
-
-double fractionalKnapsack(int W, vector<pair<int,int>> &items) {
-    sort(items.begin(), items.end(), compare);
-    double totalValue = 0.0;
-    for (auto &item : items) {
-        int value = item.first, weight = item.second;
-        if (W >= weight) {
-            W -= weight;
-            totalValue += value;
-        } else {
-            totalValue += (double)value * W / weight;
-            break;
-        }
-    }
-    return totalValue;
-}
-
 int main() {
     int n, W;
     cout << "Enter number of items: ";
@@ -34,6 +14,21 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> items[i].first >> items[i].second;
 
-    cout << "Maximum value in knapsack = " << fractionalKnapsack(W, items) << endl;
+    sort(items.begin(), items.end(), [&](pair<int,int> a, pair<int,int> b) {
+        return (double)a.first / a.second > (double)b.first / b.second;
+    });
+    
+    double totalValue = 0.0;
+    for (auto &item : items) {
+        int value = item.first, weight = item.second;
+        if (W >= weight) {
+            W -= weight;
+            totalValue += value;
+        } else {
+            totalValue += (double)value * W / weight;
+            break;
+        }
+    }
+    cout << "Maximum value in knapsack = " << totalValue << endl;
     return 0;
 }
